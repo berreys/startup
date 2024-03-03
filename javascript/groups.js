57435bc7073bf5ccc92dffecd297b8b7e94ee07a
berreys <saberrey@gmail.com>
2024-03-02 23:40:15 -0700

    groups display correctly on groups.html

--- javascript/groups.js
+++ javascript/groups.js
@@ -10,7 +10,32 @@ function loadUserName() {
 
 function loadGroups(){
     //retrieve the list of groups in the localstorage
-    localStorage.getItem('groups');
+    const groups = localStorage.getItem('groups');
     //filter to only the groups that the user is a part of
+    const deserializedGroups = JSON.parse(groups);
+    var groupsToDisplay = []
+    for(var i = 0; i < deserializedGroups.length; i++){
+        if(deserializedGroups[i].usernames.includes(localStorage.getItem('userName'))){
+            groupsToDisplay.push(deserializedGroups[i]);
+        }
+    }
     //display the groups
+    var groupUL = document.getElementById("groupUL");
+
+    groupsToDisplay.forEach(function(item) {
+        var li = document.createElement("li");
+        var link = document.createElement("a");
+        var text = document.createElement("p");
+        var button = document.createElement("button");
+        link.href = "./groupview.html";
+        link.addEventListener('click', function(){
+            localStorage.setItem('currentGroup', item);
+        })
+        button.textContent = "X";
+        text.textContent = item.id;
+        link.appendChild(text);
+        li.appendChild(link);
+        li.appendChild(button);
+        groupUL.appendChild(li);
+    });
 }
